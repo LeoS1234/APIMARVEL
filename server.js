27080541fb3b96bbef28a9374318ba7f3a08ec1b
LEO SANCHEZ <utm20040047@utma.edu.mx>
2022-07-07 11:10:35 -0500

    MARVEL API CAMBIOS PARA HEROKU

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/marvel-app'));
+app.use(express.static('./dist/marvelApp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/marvel-app/'}),
+    res.sendFile('index.html', {root: 'dist/marvelApp/'}),
 );
 
 // Start the app by listening on the default Heroku port
